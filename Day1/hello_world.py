8128966b7864cc6d8a9a9d420fc8dfa5be68c33f
Amiculay <aaronnguyen1123@gmail.com>
2022-06-04 16:22:32 -0400

    Added % format

--- Day1/hello_world.py
+++ Day1/hello_world.py
@@ -5,11 +5,13 @@ name = "Noelle"
 print("Hello", name, "!")	# with a comma -- 
 print("Hello " + name + "!")	# with a +
 # 3. print "Hello 42!" with the number in a variable
-name = 42
-print("Hello", str(name),"!")	# with a comma
-print("Hello " + str(name) + "!")	# with a +	-- this one should give us an error!
+number = 42
+print("Hello", str(number),"!")	# with a comma
+print("Hello " + str(number) + "!")	# with a +	-- this one should give us an error!
 # 4. print "I love to eat sushi and pizza." with the foods in variables
 fave_food1 = "sushi"
 fave_food2 = "pizza"
 print("I love to eat {} and {}." .format(fave_food1, fave_food2)) # with .format()
 print(f"I love to eat {fave_food1} and {fave_food2}.") # with an f string
+# Using %
+print("My name is %s and I am %d.\nNew line" % (name, number))
