e29688448e748abd38138a56af5f894047fcad2e
Amiculay <aaronnguyen1123@gmail.com>
2022-06-09 14:07:40 -0400

    Initial commit

---
+++ flask/fundamentals/hello_flask/server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo')
+def success():
+  return "Dojo!"
+
+@app.route('/say/<string:word>')
+def say_word(word):
+    return f"Hi {word}!"
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeatWord(num, word):
+    return num * word
+
+if __name__=="__main__":
+    app.run(debug=True)
