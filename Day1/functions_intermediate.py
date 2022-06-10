430481e18a275a671ac9a618bb2806f4735f73d7
Amiculay <aaronnguyen1123@gmail.com>
2022-06-10 01:32:53 -0400

    Initial commit

--- Day1/functions_intermediate.py
+++ Day1/functions_intermediate.py
@@ -1,4 +1,4 @@
-jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjx = [ [5,2,3], [10,8,9] ] 
+x = [ [5,2,3], [10,8,9] ] 
 students = [
      {'first_name':  'Michael', 'last_name' : 'Jordan'},
      {'first_name' : 'John', 'last_name' : 'Rosales'}
