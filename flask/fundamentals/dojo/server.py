1d761aac15974b482e52467ab0cfac8de329e18a
Amiculay <aaronnguyen1123@gmail.com>
2022-06-12 01:47:35 -0400

    Initial commit

--- flask/fundamentals/dojo/server.py
+++ flask/fundamentals/dojo/server.py
@@ -7,9 +7,30 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    
+    # Get data
+    strawberries = int(request.form['strawberry'])
+    blackberries = int(request.form['blackberry'])
+    raspberries = int(request.form['raspberry'])
+    apples = int(request.form['apple'])
+    count = strawberries + blackberries + raspberries + apples
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    student_id = request.form['student_id']
+    time_func = datetime.datetime.now()
+    time = time_func.strftime("%B %d %Y %I:%M:%S %p")
     print(request.form)
-    return render_template("checkout.html")
+    # Return data for post
+    return render_template("checkout.html",
+    strawberries = strawberries,
+    blackberries = blackberries,
+    raspberries = raspberries,
+    apples = apples,
+    count = count,
+    first_name = first_name,
+    last_name = last_name,
+    student_id = student_id,
+    time = time
+    )
 
 @app.route('/fruits')         
 def fruits():
